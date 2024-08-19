680fbf84b6a676228aea646bb93f14426b337dd5
mustafacelebi15 <96596130+mustafacelebi15@users.noreply.github.com>
2024-08-19 14:34:15 +0300

    Sonuç olarak bir mesaj yollama

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi.";
+
+        public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+
+        public static string MaintenanceTime = "Bakım zamanı..";
+
+        public static string ProductsListed = "Ürünler başarıyla listelendi.";
+    }
+}
